7d38a99591e2d546d553dfe409502a1a1dc4f702
Noah Paessel <knowuh@gmail.com>
2018-01-18 15:15:54 -0500

    Add inline comments with collection_id values to use on various portals.

--- src/portals/learn-stem.concord.org/has.js
+++ src/portals/learn-stem.concord.org/has.js
@@ -1,10 +1,12 @@
-// Last argument is an options hash of { limit, featured, randomize };
-// example: http://has.portal.concord.org/?prioritize=80&priority_type=external_activity
+// renderMaterialsCollection(collectionID, renderSelector, options)
+// options = { limit, featured, randomize };
 
 var params = PortalPages.ParseQueryString();
+// example: http://learn.concord.org/has?prioritize=80
 var featured = params.prioritize || params.featured;
 var options = { limit: 10, randomize: true };
 
 if (featured) options.featured = featured;
 
+// 20 is collection_id on learn.concord.org.  Use 13 for learn.staging.concord.org
 PortalPages.renderMaterialsCollection(20,'#collection-1', options);
