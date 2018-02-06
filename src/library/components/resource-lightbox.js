5fe2f627b890577e58fb31ea87fa8ad7a51e847f
Unknown <emcelroy@concord.org>
2018-02-06 15:25:21 -0500

    No suggested attribution for public domain license resources

--- src/library/components/resource-lightbox.js
+++ src/library/components/resource-lightbox.js
@@ -242,6 +242,16 @@ var ResourceLightbox = Component({
     }
 
     var license = resource.license_info;
+    var license_attribution = '';
+    // don't provide suggested attribution for public domain resources
+    if (license.code !== 'CC-BY 4.0') {
+      license_attribution = p({}, 'Suggested attribution: ' + resource.name + ' by ',
+              a({href: 'https://concord.org/'}, 'The Concord Consortium'),
+              ' is licensed under ',
+              a({href: license.deed}, license.code),
+              '.'
+            );
+    }
 
     return div({className: "portal-pages-resource-lightbox-license"},
       hr({}),
@@ -257,12 +267,7 @@ var ResourceLightbox = Component({
         br(),
         a({href: license.legal}, license.legal)
       ),
-      p({}, 'Suggested attribution: ' + resource.name + ' by ',
-        a({href: 'https://concord.org/'}, 'The Concord Consortium'),
-        ' is licensed under ',
-        a({href: license.deed}, license.code),
-        '.'
-      )
+      license_attribution
       // license.number
     );
   },
