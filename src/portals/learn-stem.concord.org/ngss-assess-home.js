8ab7fbb11d1784aa92c1c6186d7dcea629d2ff41
Unknown <emcelroy@concord.org>
2018-03-21 12:47:02 -0400

    Removed page files no longer needed for testing.

--- src/portals/learn-stem.concord.org/ngss-assess-home.js
+++ src/portals/learn-stem.concord.org/ngss-assess-home.js
@@ -1,17 +1,7 @@
 PortalPages.renderPageHeader({
   fadeIn: 100,
-  logo_class: 'cc-no-tagline',
-  oauthProviders: Portal.oauthProviders
+  logo_class: 'ngsa',
+  umbrella_link: false,
+  oauthProviders: Portal.oauthProviders,
+  theme: 'ngsa-assessment'
 }, 'page-header');
-
-jQuery(document).ready(function() {
-  jQuery('.portal-pages-main-nav-collections').text('Assessment Tasks');
-});
-
-jQuery(window).scroll(function() {
-  if (jQuery(window).scrollTop() >= 610) {
-    jQuery('.portal-pages-brand-logo-container, .portal-pages-brand-banner-bg').addClass('fixed-header');
-  } else {
-    jQuery('.portal-pages-brand-logo-container, .portal-pages-brand-banner-bg').removeClass('fixed-header');
-  }
-});
--- src/portals/learn-stem.concord.org/ngss-assess-home4.js
+++
@@ -1,7 +0,0 @@
-PortalPages.renderPageHeader({
-  fadeIn: 100,
-  logo_class: 'ngsa',
-  umbrella_link: false,
-  oauthProviders: Portal.oauthProviders,
-  theme: 'ngsa-assessment'
-}, 'page-header');
