98d1ad4c0524242d2ebcedb0179a9087cd2a96e6
Unknown <emcelroy@concord.org>
2018-04-09 14:14:51 -0400

    Added tooltip component

---
+++ src/library/components/tooltip.js
@@ -0,0 +1,61 @@
+var Component = require('../helpers/component');
+
+var div = React.DOM.div;
+var p = React.DOM.p;
+var strong = React.DOM.strong;
+var br = React.DOM.br;
+
+var Tooltip = Component({
+  getInitialState: function () {
+    return {
+      id: this.props.id,
+      text: this.props.text,
+      posx: this.props.posx,
+      posy: this.props.posy,
+      type: this.props.type || '',
+      close_delay: this.props.close_delay || 3000
+    };
+  },
+
+  getDefaultProps: function () {
+    return {};
+  },
+
+  componentWillMount: function () {
+
+  },
+
+  componentDidMount: function () {
+    this.setTimer();
+  },
+
+  componentWillUnmount: function () {
+    clearTimeout(this._timer);
+  },
+
+  setTimer: function() {
+    this._timer != null ? clearTimeout(this._timer) : null;
+
+    this._timer = setTimeout(function() {
+      jQuery('#' + this.state.id).fadeOut();
+      this._timer = null;
+    }.bind(this), this.state.close_delay);
+  },
+
+  handleClose: function (e) {
+    this.props.toggleTooltip(e);
+  },
+
+  render: function (e) {
+    var tooltip_timer;
+    return div({className: "portal-pages-tooltip-wrapper", onClick: this.handleClose},
+      div({className: "portal-pages-tooltip " + this.state.type, id: this.state.id, style: {left: this.state.posx, top: this.state.posy}, onClick: this.handleClose},
+        p({},
+          this.state.text
+        )
+      )
+    );
+  }
+});
+
+module.exports = Tooltip;
