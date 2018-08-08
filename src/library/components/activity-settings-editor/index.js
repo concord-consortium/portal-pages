cef62c8295d5491fe83997172bc9559e47ab925c
dalove1025 <dalove1025@gmail.com>
2018-08-08 15:59:23 -0400

    More WIP.

--- src/library/components/activity-settings-editor/index.js
+++ src/library/components/activity-settings-editor/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import css from './style.scss'
+// import css from './style.scss'
 
 /*
 * See `README.md` in this folder for the complete structure of the
@@ -13,14 +13,14 @@ import css from './style.scss'
  * When expanded, a DisplayJson component will show an object in it's JSON
  * form. Handy for debugging.
  *
- * NP: Don't know if this is the right way to think about a component; but, 
+ * NP: Don't know if this is the right way to think about a component; but,
  * I thought that I wanted the display of the object's JSON to change as the
  * object get's changed -- ergo, state changes. That right?
  */
 class DisplayJson extends React.Component {
-  constructor (props) {
-    super(props)
-  }
+//  constructor (props) {
+//    super(props)
+//  }
   render () {
     return (
       <div>
@@ -43,10 +43,9 @@ class TextComp extends React.Component {
     }
     return (
       <textarea
-        style={ textAreaStyle}
-        value={ this.props.value }
-        onChange={ e => this.props.update({ [this.props.name]: e.target.value}) }
-        />
+        style={textAreaStyle}
+        value={this.props.value}
+        onChange={e => this.props.update({[this.props.name]: e.target.value})} />
     )
   }
 }
@@ -56,8 +55,8 @@ class CheckboxComp extends React.Component {
     return (
       <input
         type='checkbox'
-        checked={this.props.value === 'true'}
-        onChange={ e => this.props.update({ [this.props.name]: e.target.checked ? 'true' : 'false' })}
+        checked={this.props.value === true}
+        onChange={e => this.props.update({ [this.props.name]: !!e.target.checked })}
       />
     )
   }
@@ -74,9 +73,9 @@ class CheckboxComp extends React.Component {
  * and a checkbox for a boolean, another for a label and a radio button, etc.
  */
 class SettingsPage extends React.Component {
-  constructor (props) {
-    super(props);
-  }
+//  constructor (props) {
+//    super(props);
+//  }
   render () {
     const tabPageStyle = {
       padding: '0.5em 1em',
@@ -84,9 +83,7 @@ class SettingsPage extends React.Component {
       borderTop: 'none'
     }
 
-    console.log('HERE! ' + this.props.title + this.props.fields);
-    tabPageStyle.display = this.props.selected ? 'block' : 'none';
-
+    tabPageStyle.display = this.props.selected ? 'block' : 'none'
 
     const { fields, settings, update } = this.props
     return (
@@ -95,6 +92,7 @@ class SettingsPage extends React.Component {
         <table>
           <tbody> {
             fields.map((field) => {
+              console.log(settings[field.name])
               const componentForType = (typeName) => {
                 switch (typeName) {
                   case 'boolean':
@@ -119,15 +117,65 @@ class SettingsPage extends React.Component {
   }
 }
 
+const UISettings = {
+  general: [
+    { label: 'Name', name: 'name', type: 'text' },
+    { label: 'CC Official', name: 'is_official', type: 'boolean' },
+    { label: 'Locked', name: 'is_locked', type: 'boolean' },
+    { label: 'Publication Status',
+      name: 'publication_status',
+      type: 'choice',
+      option_names: 'Published Private Other',
+      options: 'published private 42' },
+    { label: 'Copied From', name: '' },
+    { label: 'Credits', name: 'credits', type: 'text' },
+    { label: 'Sort Description', name: 'short_description', type: 'text' },
+    { label: 'Long Description', name: 'long_description', type: 'text' },
+    { label: 'Long Description for Teacher', name: 'long_description_for_teacher', type: 'text' },
+    { label: 'Teacher Guide URL', name: 'teacher_guide_url', type: 'text' },
+    { label: 'Has Pre and Post Tests', name: 'has_pretest', type: 'boolean' },
+    { label: 'Student Report Enabled', name: 'student_report_enabled', type: 'boolean' },
+    { label: 'Open URL in New Windwow', name: '' }, // Is this the "popup" flag?
+    { label: 'Run with Collaborators', name: 'allow_collaboration', type: 'boolean' },
+    { label: 'Do Not Copy', name: '' },
+    { label: 'Student Assessment Item', name: 'is_assessment_item', type: 'boolean' },
+    { label: 'Requires Download', name: '' },
+    { label: 'Projects', name: '' },
+    { label: 'Cohorts', name: '' },
+    { label: 'Grade Levels', name: '' },
+    { label: 'Subject Areas', name: '' }
+  ],
+  layout: [
+    { label: 'Display Title', name: '' },
+    { label: 'Text for Index Page', name: '' },
+    { label: 'Logo', name: '' },
+    { label: 'Thumbnail URL', name: 'thumbnail_url', type: 'text' },
+    { label: 'Theme', name: '' },
+    { label: 'LARA Project Name', name: '' },
+    { label: 'Related Content', name: '' },
+    { label: 'Estimated Time to Complete', name: '' },
+    { label: 'Activity Layout', name: '' },
+    { label: 'Authoring Mode', name: '' },
+    { label: 'Notes', name: '' }
+  ],
+  advanced: [
+    { label: 'URL', name: 'url', type: 'text' },
+    { label: 'Launch URL', name: 'launch_url', type: 'text' },
+    { label: 'External Reporting', name: '' },
+    { label: 'Custom Reporting URL', name: '' },
+    { label: 'Append Learner ID to URL', name: '' },
+    { label: 'Append Survey Monkey user ID to URL', name: '' },
+    { label: 'Enable Logging', name: '' },
+    { label: 'Save Path', name: '' }
+  ]
+}
 
 export default class ActivitySettingsEditor extends React.Component {
-
   constructor (props) {
     super(props)
-
     this.state = {
-      settingsData: Object.assign({}, props.settings),
-      UISettings: Object.assign({}, props.UISettings)
+      settingsData: Object.assign({}, props),
+      UISettings: Object.assign({}, UISettings)
     }
     this.state.UISettings.UIActivePage = 'general'
   }
@@ -143,12 +191,11 @@ export default class ActivitySettingsEditor extends React.Component {
   }
 
   activateTabPage (pageToActivate) {
-    console.log('About to activate tab-page ' + pageToActivate)
     this.updateUISettings({UIActivePage: pageToActivate})
   }
- 
+
   render () {
-   const tabBarStyle = {
+    const tabBarStyle = {
       overflow: 'hidden',
       border: '1px solid'
     }
@@ -158,9 +205,8 @@ export default class ActivitySettingsEditor extends React.Component {
       outline: 'none',
       cursor: 'pointer',
       padding: '1em',
-      margin: '0.2em',
-      background: 'orange'
-    } 
+      margin: '0.2em'
+    }
     const settings = this.state.settingsData
     const UISettings = this.state.UISettings
 
@@ -169,23 +215,36 @@ export default class ActivitySettingsEditor extends React.Component {
       <div>
         <h2>Settings</h2>
         <div style={tabBarStyle}>
-          <button style={tabButtonStyle} onClick={ () => this.activateTabPage('general') }>General</button>
-          <button style={tabButtonStyle} onClick={ () => this.activateTabPage('layout')  }>Layout</button>
-          <button style={tabButtonStyle} onClick={ () => this.activateTabPage('advanced') }>Advanced</button>
+          <button style={tabButtonStyle}
+            onClick={() => this.activateTabPage('general')}>General</button>
+          <button style={tabButtonStyle}
+            onClick={() => this.activateTabPage('layout')}>Layout</button>
+          <button style={tabButtonStyle}
+            onClick={() => this.activateTabPage('advanced')}>Advanced</button>
         </div>
-        <SettingsPage title="General Settings" update= { (nameAndValue) => this.updateSettingsData(nameAndValue) } settings={settings} fields={ UISettings.general }  selected={ UISettings.UIActivePage === 'general' } />
-
-        <SettingsPage title="Layout Settings" settings={settings} fields={ UISettings.layout }  selected={ UISettings.UIActivePage === 'layout' } />
-
-        <SettingsPage title="Advanced Settings" settings={settings} fields={ UISettings.advanced } selected={ UISettings.UIActivePage === 'advanced' } />
-        <br />
-        <br />
-        <DisplayJson title="State" objectToDisplay={settings} />
-        <br />
-        <DisplayJson title="Property" objectToDisplay={this.props} />
-     </div>
+        <SettingsPage title='General Settings'
+          update={(nameAndValue) => this.updateSettingsData(nameAndValue)}
+          settings={settings}
+          fields={UISettings.general}
+          selected={UISettings.UIActivePage === 'general'} />
+        <SettingsPage title='Layout Settings'
+          update={(nameAndValue) => this.updateSettingsData(nameAndValue)}
+          settings={settings}
+          fields={UISettings.layout}
+          selected={UISettings.UIActivePage === 'layout'} />
+        <SettingsPage title='Advanced Settings'
+          update={(nameAndValue) => this.updateSettingsData(nameAndValue)}
+          settings={settings}
+          fields={UISettings.advanced}
+          selected={UISettings.UIActivePage === 'advanced'} />
+        <div>
+          <br />
+          <br />
+          <DisplayJson title='State (edited values)' objectToDisplay={settings} />
+          <br />
+          <DisplayJson title='Property (input values)' objectToDisplay={this.props} />
+        </div>
+      </div>
     )
   }
 }
-
-
