9a7beffb5f18a94551f6dd8dec28aeb8ed11a224
dalove1025 <dalove1025@gmail.com>
2018-08-31 15:59:55 -0400

    Adding a comment.

--- src/library/components/activity-settings-editor/text-box.js
+++ src/library/components/activity-settings-editor/text-box.js
@@ -1,4 +1,19 @@
 import React from 'react'
+import { Z_VERSION_ERROR } from 'zlib';
+
+/*
+ * Note to future developers: There are two likely enhancements to this
+ * idea of a TextBox component. The first is to create another one where
+ * the underlying HTML element is specialized for the type of text being
+ * entered. E.g., <input type="url">, for components that verify the
+ * format of a URL in the browser.
+ * 
+ * The second is to create a new component supporting rich-text editing. As
+ * far as the UI goes, this can be approached by removing the "mceNoEditor"
+ * className which will, at least as of this writing in August '18, cause
+ * the tinyMCE editor to replace the textarea element. This could be a good
+ * staring point.
+ */
 
 export default class TextBox extends React.Component {
   render() {
