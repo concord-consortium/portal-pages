84e0efcd61839d3ca32f4ade5d4ec9c7dd8c5c80
Noah Paessel <knowuh@gmail.com>
2018-11-27 12:56:09 -0500

    Fix linting error.

--- src/library/helpers/make-teacher-edition-links.js
+++ src/library/helpers/make-teacher-edition-links.js
@@ -1,12 +1,13 @@
 // Selector should look like '#teacher-edition-links a'
+//
 var MakeTeacherEditionLinks = function (selector) {
   var updateAnchorTag = function (anchor) {
     var oldLink = anchor.getAttribute('href')
     var domain = encodeURIComponent('https://learn.staging.concord.org')
-    var domain_uid = Portal.currentUser.userId
+    var domainUid = Portal.currentUser.userId
     var newLink = oldLink +
       '?domain=' + domain +
-      '&domain_uid=' + domain_uid +
+      '&domain_uid=' + domainUid +
       '&mode=teacher-edition&show_index=true'
     anchor.setAttribute('href', newLink)
   }
