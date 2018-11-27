60d0debf665275d62b361ef65d0c99b22b57f979
Noah Paessel <knowuh@gmail.com>
2018-11-27 16:53:44 -0500

    `MakeTeacherEditionLinks` Add optional domain param.
    Also: Provide some documentation in the code and examples.

--- src/library/helpers/make-teacher-edition-links.js
+++ src/library/helpers/make-teacher-edition-links.js
@@ -1,12 +1,30 @@
-// Selector should look like '#teacher-edition-links a'
-//
-var MakeTeacherEditionLinks = function (selector) {
+
+// @function guessPortalDomain()
+// Tries to use window.location to extract a portal host
+// @returns string: eg: `https://learn.concord.org`
+var guessPortalDomain = function () {
+  return window.location.protocol +
+    '//' + window.location.hostname +
+    (window.location.port ? ':' + window.location.port : '')
+}
+
+// @function MakeTeacherEditionLinks(selector, domain)
+// @param `selector`: dom selector, should look like `#teacher-edition-links a`
+// @param `specDomain`: specify a host url eg: `https://learn.concord.org`
+// if `specDomain` is omited, we guess it from the window.location.
+// This function decorates <a href=""/> style links and modifies the href value.
+// It appends query params that let the runtime use login credentials specified
+// by the `specDomain` portal.
+var MakeTeacherEditionLinks = function (selector, specDomain = null) {
+  var defaultDomain = guessPortalDomain()
+  var domain = specDomain || defaultDomain
+
   var updateAnchorTag = function (anchor) {
     var oldLink = anchor.getAttribute('href')
-    var domain = encodeURIComponent('https://learn.staging.concord.org')
+    var encodedDomain = encodeURIComponent(domain)
     var domainUid = Portal.currentUser.userId
     var newLink = oldLink +
-      '?domain=' + domain +
+      '?domain=' + encodedDomain +
       '&domain_uid=' + domainUid +
       '&mode=teacher-edition&show_index=true'
     anchor.setAttribute('href', newLink)
