28508e3e26a5eeba44d0bd0db3a6292432d774f3
Scott Cytacki <scytacki@concord.org>
2019-07-30 16:16:18 -0400

    support multiple external reports on the assignments page
    this copies code from the multiple external report support in the recent activity components
    this also updates the examples so this support can be manually tested without
    connecting to a real portal

--- src/library/components/assigments/offering-details.js
+++ src/library/components/assigments/offering-details.js
@@ -6,7 +6,7 @@ import commonCss from '../../styles/common-css-modules.scss'
 
 export default class OfferingDetails extends React.Component {
   render () {
-    const { activityName, previewUrl, reportUrl, externalReport, students, reportableActivities } = this.props.offering
+    const { activityName, previewUrl, reportUrl, externalReports, students, reportableActivities } = this.props.offering
     // Activities listed in the progress table are either reportable activities or just the main offering.
     const progressTableActivities = reportableActivities || [{ id: 0, name: activityName, feedbackOptions: null }]
     return (
@@ -17,8 +17,16 @@ export default class OfferingDetails extends React.Component {
           <a href={reportUrl} target='_blank' className={commonCss.smallButton} title='Report'>Report</a>
         }
         {
-          externalReport &&
-          <a href={externalReport.url} target='_blank' className={commonCss.smallButton} title={externalReport.name}>{ externalReport.launchText }</a>
+          externalReports && externalReports.map((externalReport, index) => {
+            return (
+              <a href={externalReport.url}
+                key={index}
+                target='_blank'
+                className={commonCss.smallButton}>
+                { externalReport.launchText }
+              </a>
+            )
+          })
         }
         <div className={css.progressContainer}>
           <OfferingProgress activities={progressTableActivities} students={students} />
