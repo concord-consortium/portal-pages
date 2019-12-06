82ff58a825803961ad227e1799e5924c5ec97d6a
Unknown <emcelroy@concord.org>
2019-12-06 13:04:22 -0500

    Fix syntax problems reported by lint.

--- src/library/components/navigation/recent-collections-pages.js
+++ src/library/components/navigation/recent-collections-pages.js
@@ -5,12 +5,12 @@ export default class RecentCollectionsPages extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-        recentCollectionsPages: []
-      }
+      recentCollectionsPages: []
+    }
     this.getRecentCollectionsPages = this.getRecentCollectionsPages.bind(this)
   }
 
-  componentDidMount() {
+  componentDidMount () {
     this.getRecentCollectionsPages()
   }
 
@@ -31,23 +31,23 @@ export default class RecentCollectionsPages extends React.Component {
         <div className={css.recentCollectionsPages}>
           <h2>Recently Visited Collections</h2>
           <ul className={css.recentCollectionsPages__list}>
-          {
-            Object.keys(this.state.recentCollectionsPages).map(key => {
-              let imgStyle = {
-                backgroundImage: 'url(' + this.state.recentCollectionsPages[key].project_card_image_url + ')'
-              }
-              return (
-                <li className={css.recentCollectionsPages__list_item}>
-                  <a href={this.state.recentCollectionsPages[key].landing_page_slug}>
-                    <span className={css.recentCollectionsPages__list_item_img} style={imgStyle}></span>
-                    <span className={css.recentCollectionsPages__list_item_name}>
-                      {this.state.recentCollectionsPages[key].name}
-                    </span>
-                  </a>
-                </li>
-              )
-            })
-          }
+            {
+              Object.keys(this.state.recentCollectionsPages).map(key => {
+                let imgStyle = {
+                  backgroundImage: 'url(' + this.state.recentCollectionsPages[key].project_card_image_url + ')'
+                }
+                return (
+                  <li className={css.recentCollectionsPages__list_item}>
+                    <a href={this.state.recentCollectionsPages[key].landing_page_slug}>
+                      <span className={css.recentCollectionsPages__list_item_img} style={imgStyle} />
+                      <span className={css.recentCollectionsPages__list_item_name}>
+                        {this.state.recentCollectionsPages[key].name}
+                      </span>
+                    </a>
+                  </li>
+                )
+              })
+            }
           </ul>
         </div>
       )
