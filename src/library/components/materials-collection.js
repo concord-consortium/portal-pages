56520ca1e580227f82c9a37aa06e24d6dfc6e4d8
Unknown <emcelroy@concord.org>
2020-03-05 11:27:53 -0500

    Correctly place description within materials collection cards.

--- src/library/components/materials-collection.js
+++ src/library/components/materials-collection.js
@@ -70,8 +70,8 @@ const MaterialsCollectionItem = Component({
               {item.name}
             </a>
           </h3>
+          <div className={'portal-pages-finder-materials-collection-item__description'} dangerouslySetInnerHTML={{ __html: item.longDescription }} />
         </div>
-        <div className={'portal-pages-finder-materials-collection-item__description'} dangerouslySetInnerHTML={{ __html: item.longDescription }} />
       </div>
     )
   }
