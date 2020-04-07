df0bf2a93ae5a558cbc8303c300274eb2aa9b388
Unknown <emcelroy@concord.org>
2020-04-07 17:53:32 -0400

    Use "Collection" instead of "Project."

--- src/library/components/resource-lightbox.js
+++ src/library/components/resource-lightbox.js
@@ -434,7 +434,7 @@ var ResourceLightbox = Component({
     const resource = this.state.resource
     const showTeacherResourcesButton = this.props.showTeacherResourcesButton
     const links = resource.links
-    const isCollection = resource.material_type === 'Project'
+    const isCollection = resource.material_type === 'Collection'
     const previewButtonText = isCollection ? 'View Collection' : links.preview.text
 
     // console.log("[DEBUG] resource-lightbox links.assign_material.onclick", links.assign_material.onclick);
