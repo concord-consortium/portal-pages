31cc22609ffd36be61050f840de1dc5452d3e932
Unknown <emcelroy@concord.org>
2020-04-14 22:35:10 -0400

    Allow passing a value to parseQueryString instead of using current URL

--- src/library/helpers/parse-query-string.js
+++ src/library/helpers/parse-query-string.js
@@ -1,16 +1,18 @@
 // from https://stackoverflow.com/questions/979975/
-var parseQueryString = function () {
-  var queryString = window.location.search.replace(/^\?/, '')
-  var vars = queryString.split('&')
-  var params = {}
-  for (var i = 0; i < vars.length; i++) {
-    var pair = vars[i].split('=')
+const parseQueryString = function (queryString) {
+  if (queryString == null) {
+    queryString = window.location.search.replace(/^\?/, '')
+  }
+  let vars = queryString.split('&')
+  let params = {}
+  for (let i = 0; i < vars.length; i++) {
+    let pair = vars[i].split('=')
     // If first entry with this name
     if (typeof params[pair[0]] === 'undefined') {
       params[pair[0]] = decodeURIComponent(pair[1])
       // If second entry with this name
     } else if (typeof params[pair[0]] === 'string') {
-      var arr = [params[pair[0]], decodeURIComponent(pair[1])]
+      let arr = [params[pair[0]], decodeURIComponent(pair[1])]
       params[pair[0]] = arr
       // If third or later entry with this name
     } else {
